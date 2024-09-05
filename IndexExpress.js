435d75f5cb743fe2a6cd758de58853fbbfc2c89e
Uttam Kumar <uttammaurya377@gmail.com>
2024-09-06 02:07:23 +0530

    revision

--- IndexExpress.js
+++ IndexExpress.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path  from 'path';
+import mongoose from 'mongoose';
 
 
 const app = express();
@@ -10,6 +11,45 @@ app.set('view engine', 'ejs');
 
 //this is use for fetch data from the form ejs file
 app.use(express.urlencoded({extended:true}))
+
+// mongoose.connect()
+
+// main().catch(err => console.log(err));
+//     console.log('show error');
+
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/mern_revision');
+//   console.log('connected');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+// }
+// 
+
+mongoose.connect('mongodb://localhost:27017/Users')
+.then(()=>{
+    console.log("connected");
+})
+.catch(err => {
+    console.log("error: " + err);
+})
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+})
+const User = mongoose.model('User', UserSchema)
+
+
+
+
+
+
+
+
+
+
+
+
 
 //C R U D
 //Create = POST (method)
@@ -32,12 +72,18 @@ app.get('/',(req,res) => {
     // res.render('index.html');
 });
 
-app.post('/formData',(req,res)=>{
+app.post('/formData',async(req,res)=>{
         
     // use req.body for the save data in terminal it fetch data from the form
     console.log(req.body);
+    console.log(req.body);
+    const obj ={
+        name:req.body.name,
+        email:req.body.email,
+    }
+    await User.create(obj)
+    console.log("form summited")
     res.json({success:true});
-    
 })
 
 
