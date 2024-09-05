455f7057f55ea95b048eae1d58bd0ae010ed0f06
Uttam Kumar <uttammaurya377@gmail.com>
2024-09-05 21:26:21 +0530

    revision

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const http = require('http');
+// const http = require('http');
+import http from 'http';
 
-console.log(http);
+// console.log(http);
+const server =http.createServer();
+const port = 3000;
+server.listen(port,()=>{
+    server.listen(port,()=>{
+        console.log(`listening on port${port}`)
+    })
+})
